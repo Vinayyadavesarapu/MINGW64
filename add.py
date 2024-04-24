14ed491e9a1fd472d20fe901903ecd91f23f3e5f
Vinayyadavesarapu <159250756+Vinayyadavesarapu@users.noreply.github.com>
2024-04-24 16:38:31 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,5 +1,4 @@
 num1=1.5
 num2=2.6
-print("sum of {0} and {1} is {}[D2[C[D}".format(num1,nym2[D[u[C[C,sum))[A[A[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
-[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Csum=num1+num2
-print("sum of {0} and {1} is {2}".format(sum1,sum2,[D[D[D[D[D[Dnum[C[C[3~[3~[3~[3~[C[Cnum[C[Csum))
+sum=num1+num2
+print("sum of {0} and {1} is {2}".format(num1,num2,sum))
